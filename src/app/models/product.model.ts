1c3ff80207f98b9c68d2dcd28902fe1f2137c440
NawfelHilal <nawfelhilal13@gmail.com>
2023-12-12 14:29:00 +0100

    model product

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,27 @@
+export class Product {
+  public marque!: string;
+  public modele!: string;
+  public prix!: number;
+  public imageUrl!: string;
+  public likes: number = 0;
+  public btnValue!: string;
+  public size?: Array<string>;
+
+  constructor(
+    marque: string,
+    modele: string,
+    prix: number,
+    imageUrl: string,
+    likes: number,
+    btnValue: string,
+    size?: Array<string>
+  ) {
+    this.marque = marque;
+    this.modele = modele;
+    this.prix = prix;
+    this.imageUrl = imageUrl;
+    this.likes = likes;
+    this.btnValue = btnValue;
+    this.size = size;
+  }
+}
