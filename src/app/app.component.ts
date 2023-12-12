52af8f179ad8398cd72f01afd9fb0b32b4e3fb69
NawfelHilal <nawfelhilal13@gmail.com>
2023-12-12 14:28:52 +0100

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,57 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { ProductCardComponent } from "./product-card/product-card.component";
+import { Product } from "./models/product.model";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProductCardComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <div className="product">
+      <app-product-card
+        *ngFor="let product of products"
+        [myProduct]="product"
+      />
+    </div>
     <router-outlet></router-outlet>
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'angular-new-project';
+export class AppComponent implements OnInit {
+  title = "angular-new-project";
+  products!: Product[];
+
+  ngOnInit(): void {
+    this.products = [
+      new Product(
+        "Bmw",
+        "M3",
+        80000,
+        "https://cdn.automobile-propre.com/uploads/2023/09/BMW-M3.jpg",
+        0,
+        "Like",
+        ["XS", "S", "M", "L"]
+      ),
+      new Product(
+        "Mercedes",
+        "CLA45s",
+        80000,
+        "https://cdn.automobile-propre.com/uploads/2023/09/BMW-M3.jpg",
+        0,
+        "Like",
+        ["XS", "S", "M", "L"]
+      ),
+      new Product(
+        "AUDI",
+        "RS6",
+        80000,
+        "https://cdn.automobile-propre.com/uploads/2023/09/BMW-M3.jpg",
+        0,
+        "Like",
+        ["XS", "S", "M", "L"]
+      ),
+    ];
+  }
 }
